6505fea3335ba8572d7ab4b35ea3c7047d9c0b99
Ian Connolly <ian@connolly.io>
2014-12-15 12:52:38 +0000

    Server loop

--- lib/distributed_file_server.rb
+++ lib/distributed_file_server.rb
@@ -1,6 +1,23 @@
 require "distributed_file_server/version"
+require 'socket'
+require 'threadpool'
 
 module DistributedFileServer
   class Server
+    
+    def self.run(options)
+      pool = Threadpool::Threadpool.new workers: 4
+      server = TCPServer.new("localhost", options[:port])
+      puts "Listening on port: #{options[:port]}"
+      loop do
+        server.accept do |client|
+          pool.add_task(self.process_request, client, options[:peers])
+        end
+      end
+    end
+
+    def self.process_request(client, peers)
+      puts "Talking to client: #{client}"
+    end
   end
 end
